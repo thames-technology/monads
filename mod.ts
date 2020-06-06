3d776d7b50a723990418d9a8316d4c21412a0d87
Slavo Vojacek <public@slavovojacek.com>
2020-06-06 16:08:00 +0100

    chore(Deno): Add support for deno

---
+++ mod.ts
@@ -0,0 +1,10 @@
+export {
+  Either,
+  Left,
+  Right,
+  isLeft,
+  isRight,
+  isEither,
+} from "./src/either/either";
+export { Option, Some, None, isSome, isNone, isOption } from "./src/option/option";
+export { Result, Ok, Err, isOk, isErr, isResult } from "./src/result/result";
