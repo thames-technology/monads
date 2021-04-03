a8a39a81ea1dd6f10ab3549c03175616663279cb
Piyotaro, Kiiroi (きいろい ぴよ太郎) <piyota6@gmail.com>
2021-04-03 22:18:46 +0900

    Add support for the --isolatedModules flag in deno's default setting.

--- mod.ts
+++ mod.ts
@@ -1,3 +1,6 @@
-export { Either, Left, Right, isLeft, isRight } from "./lib/either/either.deno.ts";
-export { Option, Some, None, isSome, isNone } from "./lib/option/option.deno.ts";
-export { Result, Ok, Err, isOk, isErr } from "./lib/result/result.deno.ts";
+export { Left, Right, isLeft, isRight } from "./lib/either/either.deno.ts";
+export type { Either } from "./lib/either/either.deno.ts";
+export { Some, None, isSome, isNone } from "./lib/option/option.deno.ts";
+export type { Option } from "./lib/option/option.deno.ts";
+export { Ok, Err, isOk, isErr } from "./lib/result/result.deno.ts";
+export type { Result } from "./lib/result/result.deno.ts";
