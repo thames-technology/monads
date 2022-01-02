7c7bd09558dcc4388cadfaf9f5c49b0c103234df
Slavo Vojacek <public@slavovojacek.com>
2022-01-03 00:47:28 +0100

    chore: update deps

--- deno/index.ts
+++ deno/index.ts
@@ -1,8 +1,8 @@
-export type { Either } from "./either/either.ts";
-export { isLeft, isRight, Left, Right } from "./either/either.ts";
+export type { Either } from './either/either.ts';
+export { isLeft, isRight, Left, Right } from './either/either.ts';
 
-export type { Option } from "./option/option.ts";
-export { isNone, isSome, None, Some } from "./option/option.ts";
+export type { Option } from './option/option.ts';
+export { isNone, isSome, None, Some } from './option/option.ts';
 
-export type { Result } from "./result/result.ts";
-export { Err, isErr, isOk, Ok } from "./result/result.ts";
+export type { Result } from './result/result.ts';
+export { Err, isErr, isOk, Ok } from './result/result.ts';
